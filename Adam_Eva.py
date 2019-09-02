3a1644e72e800d68f5f9242f537863f36c571565
Yaroslav Dribniuk <yadribniuk@gmail.com>
2019-09-02 17:10:46 +0300

    Complete

---
+++ Adam_Eva.py
@@ -0,0 +1,16 @@
+class Human(object):
+    def __init__(self):
+        pass
+
+class Man(Human):
+    def __init__(self, name):
+        self.name = name
+
+class Woman(Human):
+    def __init__(self, name):
+        self.name = name
+
+def God():
+    adam = Man('Adam')
+    eva = Woman('Eva')
+    return [adam, eva]
